3c4b12fbe5e58318abbd438890286b68cd1f9f76
E2GM58 <101752844+E2GM58@users.noreply.github.com>
2022-03-20 06:40:04 -0500

    Add files via upload

---
+++ visual/templates/templatesGonzalez/Footer.php
@@ -0,0 +1,38 @@
+<!-- footer -->
+<footer class="pie-pagina">
+  <div class="grupo-1">
+    <div class="box">
+      <figure>
+        <a href="#">
+          <img src="assets/images/icono.jpg" alt="logo" />
+        </a>
+      </figure>
+    </div>
+    <div class="box">
+      <h2>SOBRE NOSOTROS</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, praesentium.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, praesentium.</p>
+    </div>
+    <div class="box">
+      <h2>SIGUENOS</h2>
+      <div class="red-social">
+        <a href="https://www.facebook.com"><img src="assets/images/face.png" alt="RF" height="80" width="100" /></a>
+        <a href="https://twitter.com"><img src="assets/images/tw.png" alt="RT" height="80" width="100" /></a>
+        <a href="https://www.instagram.com"><img src="assets/images/insta.png" alt="RC" height="80" width="100" /></a>
+      </div>
+    </div>
+  </div>
+  <div class="grupo-2">
+    <small>&copy; 2022 <b>ABJER</b> - Todos los derechos resservados.</small>
+  </div>
+</footer>
+
+<!-- Bootstrap JS: Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+  integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+  integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script src="assets/js/PerezRenan.js"></script>
+</body>
+
+</html>
